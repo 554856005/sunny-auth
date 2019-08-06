f4a4dc94f53b13f0832a666b77d80b4543c1f947
lijunsong <554856005@qq.com>
2019-08-06 15:02:25 +0800

    sunny-auth

---
+++ src/main/java/com/sunny/allauth/SunnyAuthApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.allauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunnyAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunnyAuthApplication.class, args);
+    }
+
+}
