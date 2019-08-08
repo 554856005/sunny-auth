0d7c161815e6d9d385280f052315ad218eafb83f
jasonli <2008jaylijun@sina.com>
2019-08-09 00:50:47 +0800

    shiro全代码启用securityManager

--- src/test/java/com/sunny/allauth/NonConfigFileTest.java
+++ src/test/java/com/sunny/allauth/NonConfigFileTest.java
@@ -1,7 +1,17 @@
-package test.java.com.sunny.allauth;
+package com.sunny.allauth;
 
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.pam.AtLeastOneSuccessfulStrategy;
+import org.apache.shiro.authc.pam.ModularRealmAuthenticator;
+import org.apache.shiro.authz.ModularRealmAuthorizer;
+import org.apache.shiro.authz.permission.WildcardPermissionResolver;
 import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
+import org.apache.shiro.subject.Subject;
+
 
 /**
  * NonConfigFileTest
@@ -15,5 +25,35 @@ public class NonConfigFileTest {
     public void Test() {
         DefaultSecurityManager securityManager = new DefaultSecurityManager();
 
+        //authenticator设置
+        ModularRealmAuthenticator authenticator = new ModularRealmAuthenticator();
+        authenticator.setAuthenticationStrategy(new AtLeastOneSuccessfulStrategy());
+        securityManager.setAuthenticator(authenticator);
+
+        //authorizer设置
+        ModularRealmAuthorizer authorizer = new ModularRealmAuthorizer();
+        authorizer.setPermissionResolver(new WildcardPermissionResolver());
+        securityManager.setAuthorizer(authorizer);
+
+        //Realm设置
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.driver");
+        dataSource.setUrl("jdbc:mysql:/localhost:3306/shrio");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+
+        JdbcRealm jdbcRealm = new JdbcRealm();
+        jdbcRealm.setDataSource(dataSource);
+        jdbcRealm.setPermissionsLookupEnabled(true);
+        securityManager.setRealm(jdbcRealm);
+
+        //SecurityManager设置到SecurityUtils 方便全局使用
+        SecurityUtils.setSecurityManager(securityManager);
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("JasonLi","11111");
+        subject.login(token);
+
+        Assert.assertTrue(subject.isAuthenticated());
     }
 }
