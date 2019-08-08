fd8473aa71b6f8b4ffdde06e66a166e555dc6318
jasonli <2008jaylijun@sina.com>
2019-08-09 00:34:13 +0800

    更新gradle版本，配置阿里maven仓库

---
+++ src/test/java/com/sunny/allauth/NonConfigFileTest.java
@@ -0,0 +1,19 @@
+package test.java.com.sunny.allauth;
+
+
+import org.apache.shiro.mgt.DefaultSecurityManager;
+
+/**
+ * NonConfigFileTest
+ *
+ * @Description
+ * @Author JasonLi
+ * @Date 2019-08-08 22:51
+ */
+public class NonConfigFileTest {
+    @Test
+    public void Test() {
+        DefaultSecurityManager securityManager = new DefaultSecurityManager();
+
+    }
+}
